d152bcbbf60ba1607d8f38a1104834142ad75352
4-TUNE <61144719+4-TUNE@users.noreply.github.com>
2020-05-27 15:52:55 +0300

    Добавьте файлы проекта.

---
+++ lab2.3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("N=");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];
+            ////////////////////////
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Виводим масив =");
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                Console.Write(a[i] + " ");
+            }
+
+
+
+            int min = a.Min(t => Math.Abs(t));
+            Console.WriteLine("Мiнiмальний елемент по модулю = {0}", min, Array.IndexOf(a, Math.Abs(min)));
+
+
+            int last = 0;
+            int first = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] == 0)
+                {
+                    first = i;
+                    break;
+                }
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (a[i] == 0)
+                {
+                    last = i;
+                    break;
+                }
+            }
+            int sum = 1;
+            for (int i = first + 1; i < last; i++)
+
+            {
+                sum = sum * a[i];
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
